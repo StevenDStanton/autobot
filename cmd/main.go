d62395c38c8544319472387cbd31d77b7d41551a
sstanton <steven@thesimpledev.com>
2024-07-16 20:06:11 -0400

    Added env file

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,35 @@
 package main
 
-import server "github.com/StevenDStanton/autobotgo/internal/interfaces"
+import (
+	"fmt"
+	"os"
+
+	openai "github.com/StevenDStanton/openaigo"
+	"github.com/StevenDStanton/openaigo/chat"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	server.StartServer()
+	//server.StartServer()
+	err := godotenv.Load()
+	if err != nil {
+		panic("We really need that .env file...")
+	}
+	openAiApiKey := os.Getenv("OPENAI_APIKEY")
+	client := openai.NewOpenAIClient(openAiApiKey)
+	message := chat.Message{
+		Content: "Tell me about golang",
+		Role:    chat.User,
+	}
+
+	messages := []chat.Message{message}
+	response, err := client.Chat.NewChatRequest(chat.GPT4o, messages, chat.Text)
+
+	if err != nil {
+		fmt.Println(fmt.Errorf("error: %w", err))
+		return
+	}
+
+	fmt.Printf("%v", response)
+
 }
